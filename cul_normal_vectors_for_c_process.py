ca727872b9279d36f315bdb3e3bce86ef4e702ce
KentaShibuta <q237038k@mail.kyutech.jp>
2019-07-15 15:49:54 +0900

    create connection of c process

---
+++ cul_normal_vectors_for_c_process.py
@@ -0,0 +1,126 @@
+import numpy as np
+import math
+import cmath
+import matplotlib.pyplot as plt
+import linecache
+#import pandas as pd
+
+def main(data_1, num):
+    r = 10.0
+    #num = 40
+    print(data_1)
+    
+    for i in range(0, num):
+        if i == 0:
+            a = np.array([data_1[num - 1][0],data_1[num - 1][1]])
+        else:
+            a = np.array([data_1[i-1][0],data_1[i-1][1]])
+        b = np.array([data_1[i][0],data_1[i][1]]) 
+        if i == num - 1:
+            c = np.array([data_1[0][0],data_1[0][1]])
+        else:
+            c = np.array([data_1[i+1][0],data_1[i+1][1]])
+        #print("aaaaaa")
+        print(a)
+        print(b)
+        print(c)
+
+        ba = a - b
+        ab = b - a
+        bc = c - b
+
+        ab_c = ab[0] + ab[1]*1.0j
+        bc_c = bc[0] + bc[1]*1.0j
+
+        gamma_h = 2.0 * (ab_c * bc_c)/(ab_c + bc_c)
+        gamma_vec = np.array([np.real(gamma_h), np.imag(gamma_h)])
+        rad = np.pi / 2.0
+        rot = -1.0 * np.array([[np.cos(rad), -np.sin(rad)],
+                    [np.sin(rad), np.cos(rad)]])
+        normal_gamma = np.dot(rot, gamma_vec)
+        unit_normal_gamma = normal_gamma / np.linalg.norm(normal_gamma)
+
+        # out put of normal_vector to file
+
+        with open('normal.txt', 'a') as f_handle:
+            np.savetxt(f_handle, [unit_normal_gamma], delimiter=" ")
+        print(unit_normal_gamma.dtype)
+
+        cross = np.cross(ba,bc)
+        n_ba = np.linalg.norm(ba, ord=2)
+        n_bc = np.linalg.norm(bc, ord=2)
+        vec_add = ab + bc
+        n_vec_add = np.linalg.norm(vec_add, ord=2)
+        sin = math.fabs(cross) / (n_ba * n_bc)
+        kappa = (2.0 * sin)/(math.fabs(n_vec_add))
+        #kappa = (2.0 * sin)/(n_ba + n_bc)
+        R = 1.0 / kappa
+
+        print("-----------------------------------------")
+        print("culcuration unit_normal_vector")
+        print("gamma_h =")
+        print(gamma_h)
+        print("gamma_vec =")
+        print(gamma_vec)
+        print("normal_gamm =")
+        print(normal_gamma)
+        print("unit_normal_gamma =")
+        print(unit_normal_gamma)
+
+        print("-----------------------------------------")
+        print("norm of vector from O to  circul point ")
+        print("n_a = %f" % np.linalg.norm(a, ord=2))
+        print("n_b = %f" % np.linalg.norm(b, ord=2))
+        print("n_c = %f" % np.linalg.norm(c, ord=2))
+
+
+        print("-----------------------------------------")
+        print("on circul vector")
+        print("ab = ")
+        print(ab)
+        print("bc = ")
+        print(bc)
+
+        print("-----------------------------------------")
+        print("cross of vector_ba and vector_bc")
+        print("cross = %f" % cross)
+
+        print("-----------------------------------------")
+        print("sin of vector_ba and vector_bc")
+        print("sin = %f" % sin)
+
+        print("-----------------------------------------")
+        print("curvatrue")
+        print("kappa = %f" % kappa)
+
+        print("-----------------------------------------")
+        print("curvatue radius")
+        print("R = %f" % R)
+
+        plt.quiver(b[0],b[1],unit_normal_gamma[0],unit_normal_gamma[1],angles='xy',scale_units='xy',scale=1)
+        #plt.quiver(a[0],a[1],ab[0],ab[1],angles='xy',scale_units='xy',scale=1)
+        #plt.quiver(b[0],b[1],bc[0],bc[1],angles='xy',scale_units='xy',scale=1)
+    plt.subplot().set_aspect('equal')
+    # グラフ表示
+    plt.xlim([-(r+2), r+2])
+    plt.ylim([-(r+2), r+2])
+    plt.grid()
+    plt.draw()
+    plt.show()
+
+    
+
+if __name__ == '__main__':
+    num_str = linecache.getline('data_1.txt', 1)
+    num = int(num_str)
+    print(num)
+
+    data_1 = np.loadtxt("data_1.txt",       # 読み込みたいファイルのパス
+                  delimiter=" ",    # ファイルの区切り文字
+                  skiprows=1,       # 先頭の何行を無視するか（指定した行数までは読み込まない）
+                  usecols=(0,1), # 読み込みたい列番号
+                  dtype='float64'
+                 )
+    main(data_1, num)
+
+    
---
+++ from_c_to_python.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(void)
+{
+    int num = 40;
+    int R = 10;
+
+    int theta_division = 20;
+
+    int i;
+
+    double data[num][2];
+    for(i = 0; i < theta_division; i++){
+        data[i][0] = R * cos((2.0 * M_PI) /(1.0 * theta_division)*i);
+        data[i][1] = R * sin((2.0 * M_PI) /(1.0 * theta_division)*i);
+        //printf("%d, %23.15e %23.15e\n", i, data[i][0], data[i][1]);
+    }
+
+    i = 0;
+
+    FILE *fp;
+    int c;
+    fp = fopen( "data_1.txt", "a");
+    fprintf(fp, "%d\n", theta_division);	
+
+
+        /* ファイルを開くのに失敗したら */
+        /* プログラムを終了して、シェルに戻る */
+    for(i = 0; i < theta_division; i++){
+        //printf("%d, %23.15e %23.15e\n", i, data[i][0], data[i][1]);
+        fprintf(fp, "%f %f\n", data[i][0], data[i][1]);			
+    }
+    fclose(fp);                       /* ファイルを閉じる */
+
+
+    // pythonのプログラムを呼び出し
+    system("python3 test5.py");
+    system("rm data_1.txt");//cによって生成された座標データを削除
+
+    double normal_vec[num][2];//これはグローバルで宣言をしておく．
+    fp = fopen("normal.txt", "r");//pythonのプログラムによって算出されたnormal_vectorのデータを配列に読み込む
+    for(i =0; i < num; i++){
+        fscanf(fp, "%lf, %lf", &normal_vec[i][0], &normal_vec[i][1]);
+    }
+
+    for(i =0; i < num; i++){
+        printf("%f, %f\n", normal_vec[i][0], normal_vec[i][1]);
+    }
+    system("rm normal.txt");//pythonによって生成されたnormal_vectorのデータを削除
+
+
+    return 0;
+}
